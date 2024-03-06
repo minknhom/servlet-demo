18f979928b1f9ba5bd24bd39f841219cf56e6dd9
Nguyễn Nhật Minh <91058535+minknhom@users.noreply.github.com>
2024-03-07 06:08:53 +0700

    Add files via upload

---
+++ ServletDemo/src/demo/HelloServlet.java
@@ -0,0 +1,44 @@
+package demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public HelloServlet() {
+	}
+
+	// http://localhost:8081/ServletDemo/hello
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ServletOutputStream out = response.getOutputStream();
+		String urlDB = request.getServletContext().getInitParameter("ConnectionString");
+
+		out.println("<html>");
+		out.println("<head><title>Hello Servlet</title></head>");
+
+		out.println("<body>");
+		out.println("<h3>Hello World</h3>");
+		out.println("This is my first Servlet<br/>");
+		out.println(urlDB);
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
